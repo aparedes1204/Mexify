abd86c16c722bc857071c087c8a2b92ee7e09bf4
EMendibil <83004847+EMendibil@users.noreply.github.com>
2021-12-06 18:03:51 +0100

    Estiloentzat .css fitxategia sortuta

--- php/index.php
+++ php/index.php
@@ -3,11 +3,12 @@
 <head>
     <script src='../js/jquery-3.4.1.min.js'></script>
     <script type='text/javascript' src='../js/ShowComments.js'></script>
+    <link rel="stylesheet" type="text/css" href="../css/estiloa.css">
 </head>
 <body>
     <div id="content">
-    <button type="button" id="showComments" name="1">comments of 1</button>
-    <button type="button" id="showComments" name="2">comments of 2</button>
+    <button type="button" id="showComments" class="botoiUrdin" name="1">comments of 1</button>
+    <button type="button" id="showComments" class="botoiUrdin" name="2">comments of 2</button>
     </div>
 </body>
 </html>
